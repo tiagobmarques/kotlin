7675d6f8a6306e0e1330993366b1e34b0161ad15
Tiago Marques <tiagobm564@gmail.com>
2022-01-21 04:00:25 +0400

    Added loops examples!

---
+++ loops/src/iterators.kt
@@ -0,0 +1,23 @@
+class Animal(val name: String)
+
+class Zoo(val animals: List<Animal>) {
+    operator fun iterator(): Iterator<Animal> {             // 1
+        return animals.iterator()                           // 2
+    }
+}
+
+fun main() {
+
+    val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+
+    for (animal in zoo) {                                   // 3
+        println("Watch out, it's a ${animal.name}")
+    }
+
+//    val zooList = listOf(Animal("zebra"), Animal("lion"))
+//
+//    for (animal in zooList) {                                   // 3
+//        println("Watch out, it's a ${animal.name}")
+//    }
+
+}
